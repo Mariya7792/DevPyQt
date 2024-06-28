956cc56f3e3838c6369442718495313bf49acd77
Mariya7792 <sophi5@yandex.ru>
2024-06-28 16:33:21 +0300

    Итоговое задание (редакция 1.5)

--- notes_code.py
+++ notes_code.py
@@ -1,5 +1,5 @@
 import datetime
-
+import json
 from PySide6 import QtCore, QtWidgets, QtGui
 from notes import Ui_MainWindow
 import json
@@ -24,11 +24,13 @@ class Window(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self._initSignals()
         self.create_note()
-        self.saved_notes = {}
+        self.add_note()
+        # self.saved_notes = {}
     def create_note(self):
-        with open("data.json", "w", encoding="utf-8") as file:
-            self.file = {"saved_notes": self.saved_notes}
-            json.dump(self.file, file)
+        pass
+        # with open("data.json", "w", encoding="utf-8") as file:
+        #     self.file = {"saved_notes": self.saved_notes}
+        #     json.dump(self.file, file)
 
     def _initSignals(self):
         self.ui.AddButton.clicked.connect(self.add_note)
@@ -37,21 +39,24 @@ class Window(QtWidgets.QMainWindow):
     def add_note(self):
         # input_text = self.ui.NotesText.textChanged()
         time_added = datetime.datetime.now()
-        data_to_deadline = self.ui.DeadlineData() - time_added
+        data_to_deadline = self.ui.DeadlineData.dateTime()
         self.notes_dict = {"date": time_added,
-                           "text": self.ui.NotesText.textChanged(),
-                           "deadline": self.ui.DeadlineData()
+                           "text": self.ui.NotesText.setPlainText,
+                           "deadline": self.ui.DeadlineData.dateTime(),
+                           "time until deadline": data_to_deadline
                            }
+        self.draw_list_menu()
         with open("data.json", "w", encoding="utf-8") as file:
-            self.file = {}
-        pass
-        # self.ui.textEdit.
-
+            self.file = {'notes_dict': self.notes_dict}
     def change_note(self):
         pass
         # self.ui.
     def delete_note(self):
         pass
+    def draw_list_menu(self):
+        self.ui.NotesStorage.clear()
+        for element in list(self.notes_dict):
+
 
 if __name__ ==  "__main__":
     app = QtWidgets.QApplication()
